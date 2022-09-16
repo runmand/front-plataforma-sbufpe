95446b65414c6e2edb34a962af7e1ae4e5fd1ebb
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-09-16 19:24:51 -0300

    feat(components): Improve Question

--- pages/components/Question.tsx
+++ pages/components/Question.tsx
@@ -1,17 +1,21 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, colors, Typography } from '@mui/material';
 import { ChangeEvent } from 'react';
 import SingleChoice from './answers/SingleChoice';
 
-export default function Question(props: { question: question; handleChange: (event: ChangeEvent<HTMLInputElement>, value: string) => void }) {
+export default function Question(props: { questions: question[]; handleChange: (event: ChangeEvent<HTMLInputElement>, value: string) => void }) {
   return (
-    <Card style={{ marginBottom: 8, backgroundColor: '#6D141A' }}>
-      <CardContent style={{ padding: 8 }}>
-        <Typography color={'#fff'} fontSize={8} marginBottom={'8px'}>
-          {props.question.title}
-        </Typography>
-        <SingleChoice choices={props.question.choices} onChange={props.handleChange} />
-      </CardContent>
-    </Card>
+    <div>
+      {props.questions.map((question, index) => (
+        <Card key={index} style={{ marginBottom: 8, backgroundColor: '#6D141A' }}>
+          <CardContent style={{ padding: 8 }}>
+            <Typography style={{ marginBottom: '8px', fontSize: '8px', color: colors.amber[200] }}>
+              {index} - {question.title}
+            </Typography>
+            <SingleChoice choices={question.choices} onChange={props.handleChange} />
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   );
 }
 
