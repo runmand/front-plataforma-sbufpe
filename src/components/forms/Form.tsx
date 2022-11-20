d21bbb79630bd6268a835db3067c43309d0ee51b
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-11-20 18:25:24 -0300

    add(forms): Answers

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -1,13 +1,19 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { QuestionAnswer } from '../questions/contract';
 import QuestionCard from '../questions/Question';
 import { FormState } from './contract';
 
 export default function Form() {
-	const [form, setForm] = React.useState<FormState>({ id: null, title: null, completionMessage: null, questions: [] });
-	const handleAnswerQuestion = (value: QuestionAnswer) => console.log('event-forms: ', value); //TODO: Implementar evento de troca.
+	const [form, setForm] = useState<FormState>({ id: null, title: null, completionMessage: null, questions: [] });
+	const [answers, setAnswer] = useState<QuestionAnswer[]>([]);
+	const handleAnswerQuestion = (value: QuestionAnswer) => {
+		const indexToUpdate = answers.findIndex(item => item.formQuestionFormRegisterId === value.formQuestionFormRegisterId);
+
+		if (indexToUpdate >= 0) answers[indexToUpdate] = value;
+		else answers.push(value);
+	};
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		console.log('clicou em enviar'); //TODO: Implementar envio.
@@ -41,7 +47,7 @@ export default function Form() {
 							{form.questions.map((question, index) => (
 								<QuestionCard
 									key={index}
-                  index={index}
+									index={index}
 									question={question}
 									onAnswerQuestion={handleAnswerQuestion}
 								/>
