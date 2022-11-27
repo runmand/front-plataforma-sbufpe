eac7eb3014d33b6b4eb625136622cd301738efef
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-11-27 02:24:25 -0300

    add(components): Log-in button onclick function.

--- src/components/log-in-button/index.tsx
+++ src/components/log-in-button/index.tsx
@@ -1,6 +1,16 @@
 import Button from '@mui/material/Button/Button';
 import { button } from './style';
+import { TProps } from './type';
 
-export default function Index() {
-	return <Button style={button}>Log in</Button>;
+export default function Index(props: TProps) {
+	return (
+		<Button
+			style={button}
+			onClick={() => {
+				props.onClick();
+			}}
+		>
+			Log in
+		</Button>
+	);
 }
