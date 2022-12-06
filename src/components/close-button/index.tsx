28f382805dfc2d71068c6624aac9dba75f998e18
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-06 16:44:45 -0300

    add(components): Close button.

---
+++ src/components/close-button/index.tsx
@@ -0,0 +1,17 @@
+import { TProps } from './type';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { buttonHoverStyle, buttonStyle } from './style';
+import React from 'react';
+
+export default function Index(props: TProps) {
+	const [isHover, setIsHover] = React.useState(false);
+
+	return (
+		<HighlightOffIcon
+			onClick={() => props.onClick}
+			onMouseEnter={() => setIsHover(true)}
+			onMouseLeave={() => setIsHover(false)}
+			style={isHover ? buttonHoverStyle : buttonStyle}
+		/>
+	);
+}
---
+++ src/components/close-button/style.ts
@@ -0,0 +1,17 @@
+import { positions } from '@mui/system';
+import { CSSProperties } from 'react';
+import { theme } from 'src/core/theme';
+
+export const buttonStyle: CSSProperties = {
+	color: theme.white,
+	fontSize: '2rem',
+	alignSelf: 'center',
+	position: 'absolute',
+	right: 0,
+};
+
+export const buttonHoverStyle: CSSProperties = {
+	...buttonStyle,
+	color: theme.hoverText,
+	cursor: 'pointer',
+};
