8b9f15e0143a249f3da34a03d2a11bb1afbf81e9
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-06 01:09:15 -0300

    add(core): Theme colors.

--- src/core/colors.ts
+++ src/core/colors.ts
@@ -1,4 +1,8 @@
 export const primaryColor = '#6D141A';
 export const secundaryColor = '#921c22';
+export const hoverText = '#9CFF2E';
+export const transparent = '#00000000';
 export const black = '#000';
 export const blue = '#00F';
+export const green = '#0F0';
+export const white = '#FFF';
--- src/core/theme.ts
+++ src/core/theme.ts
@@ -1,25 +1,37 @@
 import createTheme from '@mui/material/styles/createTheme';
 import React from 'react';
-import { black, blue, primaryColor, secundaryColor } from './colors';
+import { black, blue, green, hoverText, primaryColor, secundaryColor, transparent, white } from './colors';
 
 declare module '@mui/material/styles' {
 	interface Theme {
 		primaryColor: React.CSSProperties['color'];
 		secundaryColor: React.CSSProperties['color'];
+		hoverText: React.CSSProperties['color'];
+		transparent: React.CSSProperties['color'];
 		black: React.CSSProperties['color'];
 		blue: React.CSSProperties['color'];
+		green: React.CSSProperties['color'];
+		white: React.CSSProperties['color'];
 	}
 	interface ThemeOptions {
 		primaryColor: React.CSSProperties['color'];
 		secundaryColor: React.CSSProperties['color'];
+		hoverText: React.CSSProperties['color'];
+		transparent: React.CSSProperties['color'];
 		black: React.CSSProperties['color'];
 		blue: React.CSSProperties['color'];
+		green: React.CSSProperties['color'];
+		white: React.CSSProperties['color'];
 	}
 }
 
 export const theme = createTheme({
 	primaryColor: primaryColor,
 	secundaryColor: secundaryColor,
+	hoverText: hoverText,
+	transparent: transparent,
 	black: black,
 	blue: blue,
+	green: green,
+	white: white,
 });
