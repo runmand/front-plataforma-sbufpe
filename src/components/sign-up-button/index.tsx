a5e3bae2674954e5efbc33798e6073b5651f29ff
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-08 17:16:06 -0300

    remove(path): Old sign-up button.

--- src/components/sign-up-button/index.tsx
+++
@@ -1,6 +0,0 @@
-import Button from '@mui/material/Button/Button';
-import { button } from './style';
-
-export default function Index() {
-	return <Button style={button}>Sing up</Button>;
-}
--- src/components/sign-up-button/style.ts
+++
@@ -1,7 +0,0 @@
-import { CSSProperties } from 'react';
-
-export const button: CSSProperties = {
-	color: 'white',
-  fontSize: '18px',
-  fontWeight: 'bold'
-};
