6f05d5161ae220a90478b00769c260162b7327a8
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-16 16:09:50 -0300

    add(components): Text field eye icon button.
    Botão para mostrar e ocultar senha em campos de senhas.

--- src/components/text-field/index.tsx
+++ src/components/text-field/index.tsx
@@ -1,16 +1,28 @@
 import { TextField } from '@mui/material';
 import { textField } from './style';
 import { TProps } from './type';
+import IconButton from '@mui/material/IconButton';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import React from 'react';
 
-//TODO: Implementar icon para ver senha caso seja um campo do tipo password
-export default function index(props: TProps) {
+export default function Index(props: TProps) {
+	const textType = 'text';
+	const pwdType = 'password';
+	const [isShowPwd, setIsShowPwd] = React.useState<boolean>(false);
+
 	return (
-		<TextField
-			variant='outlined'
-			label={props.title}
-			style={textField}
-			onBlur={e => props.onBlur(e.target.value)}
-			type={props.type || 'text'}
-		/>
+		<div>
+			<TextField
+				variant='outlined'
+				label={props.title}
+				style={textField}
+				onBlur={e => props.onBlur(e.target.value)}
+				type={props.type ? (props.type === pwdType ? (isShowPwd ? textType : props.type) : props.type) : textType}
+			/>
+			<IconButton onClick={() => setIsShowPwd(!isShowPwd)}>
+				{props.type === pwdType && (isShowPwd ? <VisibilityOutlinedIcon /> : <VisibilityOffOutlinedIcon />)}
+			</IconButton>
+		</div>
 	);
 }
