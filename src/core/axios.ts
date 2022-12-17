4c4dbfcb1274bf631a51a8b997f12cdb3a117d55
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-17 03:26:02 -0300

    add(axios): Generic type for use contract in request responses.

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -2,14 +2,14 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { RESPONSE } from 'src/core/types';
 
 export const http = {
-	get: (path: string, config?: AxiosRequestConfig<any>): Promise<RESPONSE> => request('get', path, config),
-	post: (path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE> => request('post', path, data, config),
-	patch: (path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE> => request('patch', path, data, config),
-	delete: (path: string, config?: AxiosRequestConfig<any>): Promise<RESPONSE> => request('delete', path, config),
+	get: <D>(path: string, config?: AxiosRequestConfig<any>): Promise<RESPONSE<D>> => request('get', path, config),
+	post: <D>(path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE<D>> => request('post', path, data, config),
+	patch: <D>(path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE<D>> => request('patch', path, data, config),
+	delete: <D>(path: string, config?: AxiosRequestConfig<any>): Promise<RESPONSE<D>> => request('delete', path, config),
 };
 
-const request = async (method: any, path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE> => {
-	const result: RESPONSE = {
+const request = async <D>(method: any, path: string, data?: any, config?: AxiosRequestConfig<any>): Promise<RESPONSE<D>> => {
+	const result: RESPONSE<D> = {
 		status: -1,
 		data: null,
 		msg: null,
@@ -20,11 +20,11 @@ const request = async (method: any, path: string, data?: any, config?: AxiosRequ
 		.then(res => {
 			result.status = res.data.status;
 			result.data = res.data.data;
-			result.errors = res.data.errors.map((err: any) => err.message);
+			result.errors = res.data.errors?.map((err: any) => err.message);
 			return result;
 		})
 		.catch(e => {
-			result.errors = e.response.data.errors.map((err: any) => err.message);
+			result.errors = e.response.data.errors?.map((err: any) => err.message);
 			return result;
 		});
 };
