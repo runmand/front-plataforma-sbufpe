aa0a26dd345fa24e2f00076bf8ed463bb2c70cef
Marcelo Dias <mdcmarcelodias@gmail.com>
2022-12-21 03:05:09 -0300

    fix(components): Appbar position.

--- src/components/app-bar/index.tsx
+++ src/components/app-bar/index.tsx
@@ -5,9 +5,5 @@ import { TPROPS } from './type';
 import { appbarStyle } from './style';
 
 export default function Index(props: TPROPS) {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar style={appbarStyle}>{props.toolbarChild}</AppBar>
-		</Box>
-	);
+	return <AppBar style={appbarStyle}>{props.toolbarChild}</AppBar>;
 }
--- src/components/app-bar/style.ts
+++ src/components/app-bar/style.ts
@@ -2,16 +2,11 @@ import { CSSProperties } from 'react';
 import { theme } from 'src/core/theme';
 
 export const appbarStyle: CSSProperties = {
-	position: 'fixed',
-	top: 0,
-	left: 0,
+  position: 'relative',
 	width: '100%',
 	height: '10%',
 	display: 'flex',
 	justifyContent: 'center',
-	alignItems: 'center',
 	padding: '1rem',
-	transition: '0.6s',
 	backgroundColor: theme.primaryColor,
-	zIndex: `${Infinity}`,
 };
