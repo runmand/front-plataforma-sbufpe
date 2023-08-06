b2972b9efca478abe781d11e04d3d9edc284cd30
Braian Fellipe <braianfelipecsk123@hotmail.com>
2023-08-05 23:52:29 -0300

    Temporary remove PDF download

--- src/components/modal/form/result/index.tsx
+++ src/components/modal/form/result/index.tsx
@@ -8,36 +8,39 @@ import { TPROPS } from './type';
 import Header from '../../header/index';
 import ActionArea from '@components/modal/action-area';
 import { theme } from 'src/core/theme';
-import { ResultFormPdf } from '@components/FormResultPdf';
-import { pdf } from '@react-pdf/renderer';
+// import { ResultFormPdf } from '@components/FormResultPdf';
+// const PdfComponent = React.lazy(() => import('@components/FormResultPdf'));
+// import { pdf } from '@react-pdf/renderer';
+
 
 export default function Index(props: TPROPS) {
+	
 	async function downloadPdf(){
-		const blob = await pdf(
-			<ResultFormPdf
-			domainList={props.formResult.domainList}
-		maxScore={props.formResult.maxScore}
-		score={props.formResult.score}	
-			/>
-		  ).toBlob();
+		// const blob = await pdf(
+		// 	<PdfComponent
+		// 	domainList={props.formResult.domainList}
+		// maxScore={props.formResult.maxScore}
+		// score={props.formResult.score}	
+		// 	/>
+		//   ).toBlob();
 	
-		  // Create a URL for the blob object
-		  const url = URL.createObjectURL(blob);
-		  // Create an anchor element and set its href to the PDF URL
-		  const a = document.createElement("a");
-		  a.href = url;
+		//   // Create a URL for the blob object
+		//   const url = URL.createObjectURL(blob);
+		//   // Create an anchor element and set its href to the PDF URL
+		//   const a = document.createElement("a");
+		//   a.href = url;
 	
-		  // Set the anchor element's download attribute to the PDF file name
-		  a.download = new Date()+"";
+		//   // Set the anchor element's download attribute to the PDF file name
+		//   a.download = new Date()+"";
 	
-		  // Append the anchor element to the document body
-		  document.body.appendChild(a);
+		//   // Append the anchor element to the document body
+		//   document.body.appendChild(a);
 	
-		  // Click the anchor element to initiate the download
-		  a.click();
+		//   // Click the anchor element to initiate the download
+		//   a.click();
 	
-		  // Remove the anchor element from the document body
-		  document.body.removeChild(a);
+		//   // Remove the anchor element from the document body
+		//   document.body.removeChild(a);
 	}
 
 	return (
