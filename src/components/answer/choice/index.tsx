d42e48d25ed98bae610fd83c132599fba0b4913b
Marcelo Arruda <arruda16.marcelo@gmail.com>
2023-08-28 15:04:18 -0300

    Implementação Autocomplete

--- src/components/answer/choice/index.tsx
+++ src/components/answer/choice/index.tsx
@@ -1,5 +1,5 @@
 import RadioGroup from '@mui/material/RadioGroup';
-import { FormControl, FormControlLabel, InputLabel, MenuItem, Radio, Select, Typography } from '@mui/material';
+import { Autocomplete, FormControl, FormControlLabel, InputLabel, MenuItem, Radio, Select, TextField, Typography } from '@mui/material';
 import { pink } from '@mui/material/colors';
 import { CHOICE, TPROPS } from './type';
 import React, { useEffect } from 'react';
@@ -24,49 +24,58 @@ export default function Index(props: TPROPS) {
 	}
 
 	if (props.choiceType === "select") {
+		const options:any = []
+		props.choices.map((choices,index)=> {options.push(choices,)})
+		{console.log(options)}
 		return (
-		  <FormControl fullWidth>
-			<InputLabel sx={{color:'#6d141a','&.Mui-focused':{color:'#6d141a'}}} id="demo-simple-select-label">Selecione</InputLabel>
-			<Select
+			<Autocomplete
+      id="combo-box-demo"
+      options={options.title}
+      sx={{ width: 1}}
+      renderInput={(params) => <TextField {...params} label="Selecione" />}
+    />
+	// 	  <FormControl fullWidth>
+	// 		<InputLabel sx={{color:'#6d141a','&.Mui-focused':{color:'#6d141a'}}} id="demo-simple-select-label">Selecione</InputLabel>
+	// 		<Select
 		
 	
-	 sx={{
+	//  sx={{
 	
-		color: "#6d141a",
-		'.MuiOutlinedInput-notchedOutline': {
-		  borderColor: '#6d141a',
-		},
-		'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-		  borderColor: '#6d141a',
-		},
-		'&:hover .MuiOutlinedInput-notchedOutline': {
-		  borderColor: '#6d141a',
-		},
-		'.MuiSvgIcon-root ': {
-		  fill: "white !important",
-		},
-		'.MuiInputLabel-root':{
-			color:'#6d141a'
-		},
-	  }}          labelId="select-label"
-			  id="select"
-			  label="Selecione"
-			>
-			  {props.choices.map((choice, index) => (
-				<MenuItem
-				key={index}
-				  onClick={() => {
-					handleSelectChoice(index, choice);
-				  }}
-				  value={choice.title}
-				>
-				  {choice.title}
-				</MenuItem>
-			  ))}
-			</Select>
-		  </FormControl>
+	// 	color: "#6d141a",
+	// 	'.MuiOutlinedInput-notchedOutline': {
+	// 	  borderColor: '#6d141a',
+	// 	},
+	// 	'&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+	// 	  borderColor: '#6d141a',
+	// 	},
+	// 	'&:hover .MuiOutlinedInput-notchedOutline': {
+	// 	  borderColor: '#6d141a',
+	// 	},
+	// 	'.MuiSvgIcon-root ': {
+	// 	  fill: "white !important",
+	// 	},
+	// 	'.MuiInputLabel-root':{
+	// 		color:'#6d141a'
+	// 	},
+	//   }}          labelId="select-label"
+	// 		  id="select"
+	// 		  label="Selecione"
+	// 		>
+	// 		  {props.choices.map((choice, index) => (
+	// 			<MenuItem
+	// 			key={index}
+	// 			  onClick={() => {
+	// 				handleSelectChoice(index, choice);
+	// 			  }}
+	// 			  value={choice.title}
+	// 			>
+	// 			  {choice.title}
+	// 			</MenuItem>
+	// 		  ))}
+	// 		</Select>
+	// 	  </FormControl>
 		);
-	  }
+	}
 
 	return (
 		<RadioGroup>
