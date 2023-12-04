71abd607cc41d8ba0d40debfee9271244cd01f18
Marcelo Arruda <arruda16.marcelo@gmail.com>
2023-12-03 21:36:58 -0300

    Inclusão dashboard CEO

--- src/components/container/ceo-data/index.tsx
+++ src/components/container/ceo-data/index.tsx
@@ -1,8 +1,34 @@
+import { Box, Typography } from '@mui/material';
 import * as React from 'react';
+import { theme } from 'src/core/theme';
 
 export default function Index() {
-  console.log("Está abrindo")
   return(
-    <h1>CEO</h1>
+    <Box
+      sx={{
+        alignContent:'center',
+        display:'flex', 
+        flexDirection:'column',
+        textAlign:'center'}}>
+      <Typography
+        variant='h4'
+        sx={{
+          color:theme.primaryColor,
+					textAlign:'center',
+					fontWeight:'bold',
+					paddingY:'2rem'}}>
+          USUÁRIOS
+      </Typography>
+      <Box>
+        <iframe
+          title="Report Section"
+          style={{
+            minHeight:'600px',
+            width:'100%'}}   
+          src="https://app.powerbi.com/view?r=eyJrIjoiZDY3NGI0MmYtOGM5NC00OWNjLTg1M2QtODA0YTFkZDVhOGYyIiwidCI6ImE2NTk5NGY3LTU1MjgtNGE4NC1iODU3LWJmMDRlMDBjNGRhNCJ9&pageName=ReportSection"
+          allow="fullscreen"></iframe>
+        </Box>
+    
+  </Box>
   )
 }
