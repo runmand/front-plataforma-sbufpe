67b080436424a33e0cbaca712c0361692b676f43
MrIgortbr <igorperecom@gmail.com>
2025-01-22 11:41:39 -0300

    next.config updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
   env: {
-    API_URL: "http://127.0.0.1:2000",
+    API_URL: process.env.API_URL,
     JWT_SECRET: process.env.JWT_SECRET,
   },
 };
