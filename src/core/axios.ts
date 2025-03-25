e665719a8e8173fb42324022039fb54a7d5e2d31
IgortBr <54614579+MrigortBr@users.noreply.github.com>
2025-03-25 19:00:26 -0300

    New Open Routes

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -6,7 +6,7 @@ export const http = axios.create({ baseURL: process.env.API_URL });
 
 http.interceptors.request.use(
 	config => {
-		const openRoutes: string[] = ['/', '/login/cpf', '/login/cellphone', '/login/email', '/login/username', '/user-registers', '/user-types', '/reset/'];
+		const openRoutes: string[] = ['/', '/login/cpf', '/login/cellphone', '/login/email', '/login/username', '/user-registers', '/user-types', '/reset/', "/biopen/aps", "/biopen/ceo", "/biopen/usuario",];
 
 		/** Se a rota for protegida, segue a lógica. */
 		if (!openRoutes.includes(config.url)) {
