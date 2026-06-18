6692cca3b05ea79a2c310a1b539d912af52ee050
MrIgortbr <igortbr0412@hotmail.com>
2026-06-18 16:28:10 -0300

    New Menu

--- src/components/menu/drawer/type.ts
+++ src/components/menu/drawer/type.ts
@@ -1,7 +1,9 @@
-import { MENU_ITEM } from '../items/type';
+import { MENU_ITEM } from "../items/type";
 
 export type TPROPS = {
-  isOpen: boolean;
-  menuItems: MENU_ITEM[];
-  onClose: () => void;
+    showExit?: boolean;
+    exitFunction?: () => void;
+    isOpen: boolean;
+    menuItems: MENU_ITEM[];
+    onClose: () => void;
 };
