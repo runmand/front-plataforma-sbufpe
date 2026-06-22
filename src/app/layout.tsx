035d4153ddc45d7335f6c2a48580baf56c4bf3e3
wemersonmatheuss <wemersonmthdev@gmail.com>
2026-06-22 16:59:19 -0300

    KAN-86 Reformular página Contato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pt-BR">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Newsreader:opsz,wght@6..72,300;6..72,400;6..72,700&display=swap"
+                    rel="stylesheet"
+                />
+            </head>
             <body>
                 <Providers>{children}</Providers>
             </body>
