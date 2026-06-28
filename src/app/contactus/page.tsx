65fdadbf832d082715882b13a2b6b2bf55bb2b17
Wemerson Matheus <wemersonmthdev@gmail.com>
2026-06-28 17:15:38 -0300

    KAN-86 Reformular página Contato (#207)
    * KAN-86 Reformular página Contato
    
    * KAN-86 Usa MailOutlineIcon do MUI no lugar do icone customizado

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -5,12 +5,18 @@ import Base from '@components/base-layout/index';
 import NewMenu from '@components/newMenu/index';
 import ContactUsContainer from '@components/container/contact-us';
 
+import FooterMain from '@components/footer/main/index';
+
 export default function Page() {
     useEffect(() => {
         document.title = 'Contato | GestBucal';
     }, []);
 
     return (
-        <Base appBarChild={<NewMenu />} mainContainerChild={<ContactUsContainer />} />
+        <Base
+            appBarChild={<NewMenu />}
+            mainContainerChild={<ContactUsContainer />}
+            footerChild={<FooterMain />}
+        />
     );
 }
